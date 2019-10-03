27ec8ea2de2eca4122086ee6a4a0a1dcda43371a
haifeng520 <39614671+haifeng520@users.noreply.github.com>
2019-10-03 10:57:33 +0800

    base

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+// 引入样式初始化文件
+import './stylesheets/mine.less';
+// 引入rem
+import './modules/rem';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
